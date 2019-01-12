808c78373bb666f379e5b15510ced69b699efacc
Pavithra <pavidsscst@gmail.com>
2019-01-13 02:26:02 +0530

    Add project files.

---
+++ OkloApi/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using OkloApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OkloApi.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        [HttpPost]
+        public dynamic RejectRequest(Invoice obj)
+        {
+            return Json(new { Succeeded = "true" });
+        }
+
+        [HttpPost]
+        public dynamic OkloDocumentApi(Invoice obj)
+        {
+            return Json(new { Succeeded = "true" });
+        }
+    }
+}
---
+++ OkloApi/Models/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OkloApi.Models
+{
+    public class Invoice
+    {
+        public string ReferenceId { get; set; }
+        public string Status { get; set; }
+        public string Remarks { get; set; }
+        public string ErrorType { get; set; }
+
+        public string DocumentId { get; set; }
+    }
+}
